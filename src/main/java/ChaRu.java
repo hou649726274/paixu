2c12f18c26e66df42aa7a88179d68079e58043a7
hou649726274 <aaron_hy@163.com>
2020-09-17 22:52:16 +0800

    选择排序、插入排序OK

--- src/main/java/ChaRu.java
+++ src/main/java/ChaRu.java
@@ -1,16 +1,24 @@
 public class ChaRu {
     public static void main(String[] args) {
+        //假设第一个数最小，从第下标1到9，每次下标循环都与前面的数据进行比较，如果后面的数据小于前面的数据，前面的数据进行位移操作。一旦后面的数据大于前面的数据，跳出比较，进行数组的插入。
         int[] arr = {11,3,8,5,2,6,12,7,1,2};
         int i, j;
         int n = arr.length;
         int target;
-        for(i=1;i<n-1;i++){
-            j = i;
+        for(i=1;i<n;i++){
             target = arr[i];
-            while(j>0&&target<arr[]){
-
+            for(j=i-1; j>=0 ;j--){
+                if(arr[j]>target){
+                    arr[j+1]=arr[j];
+                }else{
+                    break;
+                }
             }
+            arr[j+1]=target;
         }
 
+        for(int k : arr){
+            System.out.println(k);
+        }
     }
 }
--- src/main/java/XuanZe.java
+++ src/main/java/XuanZe.java
@@ -3,17 +3,17 @@ public class XuanZe {
     public static void main(String[] args) {
         //原理：双层遍历，每次拿循环的第一个数和其余的数进行比较，获取最小的数再和第一个数进行位置互换。再遍历循环+1的数与后面的数比较。
         int[] shuZu = {11,9,8,11,12,13};
-        int temp,index;
+        int temp,minIndex;
         for(int i=0; i<shuZu.length-1 ; i++){
-            index = i;
+            minIndex = i;
             for(int j= i+1; j <shuZu.length-1;j++){
-                if(shuZu[j] < shuZu[index]){
-                    index = j;
+                if(shuZu[j] < shuZu[minIndex]){
+                    minIndex = j;
                 }
             }
-            if(index != i ){
-                temp = shuZu[index];
-                shuZu[index] = shuZu[i];
+            if(minIndex != i ){
+                temp = shuZu[minIndex];
+                shuZu[minIndex] = shuZu[i];
                 shuZu[i] = temp;
             }
         }
